ba09b1ec500cd2fe72d7f65512b2ed5feecb7df2
Mou <peter.gerhart80@gmail.com>
2021-06-18 09:26:47 +0200

    Titel gesetzt

--- src/main/java/viergewinnt/App.java
+++ src/main/java/viergewinnt/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +21,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("startBildschirm"), 1000, 750);
+        // stage.getIcons().add(new Image(""));
+        stage.setTitle("4-Gewinnt");
         stage.setScene(scene);
         stage.show();
 
