95bddac8101d4e953c3f1290c5f6348e0ba33778
Paul <paul.huellmandel@gmail.com>
2021-06-18 17:03:43 +0200

    Neue Farbwahl

--- src/main/java/viergewinnt/Singleplayer.java
+++ src/main/java/viergewinnt/Singleplayer.java
@@ -1,57 +1,53 @@
 package viergewinnt;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Singleplayer {
+public class Singleplayer implements Initializable{
 
     @FXML
     public Button single_weiter;
-    public ColorPicker single_farbwahl2;
-    public ColorPicker single_farbwahl1;
     public TextField single_namenswahl2;
     public TextField single_namenswahl1;
     public Button single_zurück;
+    public ChoiceBox<String> single_farbwahl1;
+    public ChoiceBox<String> single_farbwahl2;
 
     /**
      * Weiterleitung zur spiel.fxml, sofern zwei unterschiedliche Namen und zwei unterschiedliche Farben ausgewählt wurden, andernfalls folgt eine Fehlermeldung
      */
-    public void singleweiter() throws IOException
-    {
-        if (single_namenswahl1.getText() == null || single_namenswahl2.getText() == null || single_namenswahl1.getText().equals(single_namenswahl2.getText()))
-        {
+    public void singleweiter() throws IOException {
+        if (single_namenswahl1.getText() == null || single_namenswahl2.getText() == null || single_namenswahl1.getText().equals(single_namenswahl2.getText())) {
             Alert keinName = new Alert(Alert.AlertType.INFORMATION);
             keinName.setTitle("Fehler");
             keinName.setContentText("Geben Sie bitte zwei unterschiedliche Namen ein!");
             keinName.showAndWait().ifPresent(rs -> {
-                if (rs == ButtonType.OK)
-                {
+                if (rs == ButtonType.OK) {
                     System.out.println("OK gedrückt.");
                 }
             });
-        }
-        else
-        {
-            if (single_farbwahl1.getValue().toString().equals(single_farbwahl2.getValue().toString()))
-            {
+        } else {
+            if (single_farbwahl1.getValue().equals(single_farbwahl2.getValue())) {
                 Alert gleicheFarbe = new Alert(Alert.AlertType.INFORMATION);
                 gleicheFarbe.setTitle("Fehler");
                 gleicheFarbe.setContentText("Wählen Sie bitte zwei unterschiedliche Farben aus!");
                 gleicheFarbe.showAndWait().ifPresent(rs -> {
-                    if (rs == ButtonType.OK)
-                    {
+                    if (rs == ButtonType.OK) {
                         System.out.println("OK gedrückt.");
                     }
                 });
-            }
-            else
-            {
-                
+            } else {
+
                 try {
                     App.setRoot("spiel");
-                    Auswahl();
+                    FarbWahlSpieler1();
+                    FarbWahlSpieler2();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -63,20 +59,154 @@ public class Singleplayer {
     /**
      * Weiterleitung zur startBildschirm.fxml
      */
-    public void singlezurück() throws IOException
-    {
+    public void singlezurück() throws IOException {
         App.setRoot("startBildschirm");
     }
 
     /**
      * Hinzufügen der zwei Spieler mit den ausgewählten Namen und Farben
-     */
-    public void Auswahl() throws Exception
+
+     public void Auswahl() throws Exception
+     {
+     VierGewinnt.zuruecksetzen();
+     VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), single_farbwahl1.getValue());
+     VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), single_farbwahl2.getValue());
+
+     }  */
+
+
+    private String[] farben = {"Rot", "Orange", "Gelb", "Grün", "Blau", "Schwarz"};
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1)
     {
-        VierGewinnt.zuruecksetzen();
-        VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), single_farbwahl1.getValue());
-        VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), single_farbwahl2.getValue());
+        single_farbwahl1.getItems().addAll(farben);
+        single_farbwahl2.getItems().addAll(farben);
+    }
+
+    public void FarbWahlSpieler1()
+    {
+        try {
+            if (single_farbwahl1.getValue() == "Rot")
+            {
+                VierGewinnt.zuruecksetzen();
+                VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), Color.RED);
+            }
+            else
+            {
+                if (single_farbwahl1.getValue() == "Orange")
+                {
+                    VierGewinnt.zuruecksetzen();
+                    VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), Color.ORANGE);
+                }
+                else
+                {
+                    if (single_farbwahl1.getValue() == "Gelb")
+                    {
+                        VierGewinnt.zuruecksetzen();
+                        VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), Color.YELLOW);
+                    }
+                    else
+                    {
+                        if (single_farbwahl1.getValue() == "Grün")
+                        {
+                            VierGewinnt.zuruecksetzen();
+                            VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), Color.GREEN);
+                        }
+                        else
+                        {
+                            if (single_farbwahl1.getValue() == "Blau")
+                            {
+                                VierGewinnt.zuruecksetzen();
+                                VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), Color.BLUE);
+                            }
+                            else
+                            {
+                                if (single_farbwahl1.getValue() == "Schwarz")
+                                {
+                                    VierGewinnt.zuruecksetzen();
+                                    VierGewinnt.spielerHinzufuegen(single_namenswahl1.getText(), Color.BLACK);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        };
 
     }
 
+    public void FarbWahlSpieler2()
+    {
+        try {
+            if (single_farbwahl2.getValue() == "Rot")
+            {
+                VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), Color.RED);
+            }
+            else
+            {
+                if (single_farbwahl2.getValue() == "Orange")
+                {
+                    VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), Color.ORANGE);
+                }
+                else
+                {
+                    if (single_farbwahl2.getValue() == "Gelb")
+                    {
+                        VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), Color.YELLOW);
+                    }
+                    else
+                    {
+                        if (single_farbwahl2.getValue() == "Grün")
+                        {
+                            VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), Color.GREEN);
+                        }
+                        else
+                        {
+                            if (single_farbwahl2.getValue() == "Blau")
+                            {
+                                VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), Color.BLUE);
+                            }
+                            else
+                            {
+                                if (single_farbwahl2.getValue() == "Schwarz")
+                                {
+                                    VierGewinnt.spielerHinzufuegen(single_namenswahl2.getText(), Color.BLACK);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        };
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
